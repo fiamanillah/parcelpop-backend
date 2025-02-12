1141ae885b73ba6b0d51ba8419261c70fd841305
fiamanillah <fiamanillahb2@gmail.com>
2025-02-12 21:28:44 +0600

    Vercel export

--- server.js
+++ server.js
@@ -76,7 +76,9 @@ app.use('/api/parcel', parcelRoutes);
 app.use('/api', userRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
